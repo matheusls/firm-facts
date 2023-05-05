09ff54b8d8058d22ec765652da471ea13cd9ae45
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2023-05-05 18:12:35 -0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Firm Facts</h1>
   </React.StrictMode>
 );
 
