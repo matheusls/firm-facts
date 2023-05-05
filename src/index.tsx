93c476895c2c6ff66246857d6089cfb1359a5b00
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2023-05-05 18:37:48 -0300

    Setup initial styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+
+import 'styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
